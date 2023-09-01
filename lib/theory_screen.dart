ea1ceb35c2852e3bee2f8eb7fd7c4bb7dfbb31e3
albu <aleksandr.buslaev@mapbox.com>
2023-09-01 22:18:57 +0300

    refactor classes to make code cleaner and more readable

--- lib/theory_screen.dart
+++ lib/theory_screen.dart
@@ -1,36 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:busylingo/practice_screen.dart';
 import 'package:busylingo/question.dart';
-import 'package:busylingo/questions_by_topic.dart';
+import 'package:busylingo/data/questions_by_topic.dart';
 
 class TheoryScreen extends StatelessWidget {
   final String topic;
 
   const TheoryScreen({super.key, required this.topic});
 
-  @override
-  Widget build(BuildContext context) {
+  List<Question> getQuestions() {
     List<Question> questions = List.of(questions_by_topic[topic] ?? []);
     questions.shuffle();
-    questions = questions.take(10).toList();
+    return questions.take(10).toList();
+  }
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Theory - $topic',
-          style: const TextStyle(
-            fontSize: 20.0, // Adjust title font size
-            fontWeight: FontWeight.bold, // Make it bold
-          ),
-        ),
-        backgroundColor: Colors.blue, // Match the app's theme color
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back), // Add a back button
-          onPressed: () {
-            Navigator.pop(context);
-          },
+  AppBar buildAppbar(BuildContext context) {
+    return AppBar(
+      title: Text(
+        'Theory - $topic',
+        style: const TextStyle(
+          fontSize: 20.0, // Adjust title font size
+          fontWeight: FontWeight.bold, // Make it bold
         ),
       ),
+      backgroundColor: Colors.blue, // Match the app's theme color
+      leading: IconButton(
+        icon: const Icon(Icons.arrow_back), // Add a back button
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+    );
+  }
+
+  Widget buildPracticeButton(BuildContext context, List<Question> questions) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          // Navigate to the practice screen.
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => PracticeScreen(
+                topic: topic,
+                questions: questions,
+              ),
+            ),
+          );
+        },
+        child: const Text('Practice'),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Question> questions = getQuestions();
+
+    return Scaffold(
+      appBar: buildAppbar(context),
       body: SingleChildScrollView(
         // Scrollable content
         padding: const EdgeInsets.all(16.0),
@@ -46,7 +74,6 @@ class TheoryScreen extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 16.0), // Add spacing between text elements
-
             Text(
               theory[topic] ?? "",
               style: const TextStyle(
@@ -54,24 +81,7 @@ class TheoryScreen extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 24.0), // Add more spacing before the button
-
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  // Navigate to the practice screen.
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => PracticeScreen(
-                        topic: topic,
-                        questions: questions,
-                      ),
-                    ),
-                  );
-                },
-                child: const Text('Practice'),
-              ),
-            ),
+            buildPracticeButton(context, questions),
           ],
         ),
       ),
